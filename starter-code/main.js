ea8eb6d45954aabe4ebcb29d3f9b501a2eb3ec24
YOUR_GITHUB_USERNAMEcd <fevillarosa73@gmail.com>
2018-05-07 23:53:50 -0700

    submit homework

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,49 +1,55 @@
-/*
-	Part 1: Create three variables: name, age, address.
-	Create a fourth variable, greeting, that combines these 3 variables
-	to form a greeting.
-	For example, "Hello, I'm Anthony, I'm 20 years old and live in 100 Main St."
-	Log this variable to the console (console.log(yourVariableName)). 
-	Check the console to make sure you're getting the desired result.
 
+
+var firstName = 'Fanny';
+var age = 45;
+var city = 'San Ramon';
+var greeting = "Hi, I'm " + firstName + "," + " I'm " + age + " years old and live in " + city + "."
+var greeting2 = `Hi, I'm ${firstName}, I am ${age} years old and live in ${city}.`
+
+var side1 = 45;
+var side2 = 90;
+var side3 = (Math.pow(side1, 2)) + (Math.pow(side2, 2))
+
+//a squared + b squared = c squared
+
+/* The Math.max() function returns the largest of zero or more numbers.
+console.log(Math.max(1, 3, 2));
+// expected output: 3
+
+The static function Math.min() returns the lowest-valued number passed into it,
+or NaN if any parameter isn't a number and can't be converted into one.
+console.log(Math.min(2, 3, 1));
+// expected output: 1
 */
-
+var highestNumber = 0;
+var numbers = [20, 3, 5, 7, 13, 30];
+var i;
+for (i = 0; i < highestNumber.length; i++) {number += highestNumber[i] + "<br>";}
+if (i < highestNumber.length) {
+    highestNumber = i;
+}
 /*
-	Part 2: Create another variable called greeting2. This time, use the
-	new ES6 Syntax, string interpolation, to create the same greeting.
 
-	Hint: String interpolation uses backticks (`) instead of quotation marks.
+The for loop has the following syntax:
 
-	Log this variable to the console (console.log(yourVariableName)). 
-	Check the console to make sure you're getting the desired result.
+for (statement 1; statement 2; statement 3) {
+    code block to be executed
+}
 
-*/
+Example
+for (i = 0; i < 5; i++) {
+    text += "The number is " + i + "<br>";
+}
 
-/*
-	Part 3: Pythagorean theorem. We will be using the Math object we saw earlier
-	in the lesson for this problem.
+From the example above, you can read:
 
-	Create 2 variables, side1 and side2, which represent two sides of a triangle.
-	Assign these variables any positive integer values.
-	Create a third variable, side3, which represents the hypotenuse of the triangle.
+Statement 1 sets a variable before the loop starts (var i = 0).
 
-	Use the Pythogorean theorem to solve for the value of side3.
-	(https://en.wikipedia.org/wiki/Pythagorean_theorem)
+Statement 2 defines the condition for the loop to run (i must be less than 5).
 
-	Hint: Take a look at the MDN reference for more information about the Math object.
-	(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math)
+Statement 3 increases a value (i++) each time the code block in the loop has been executed.
 
-	Hint: We need to be able to calculate the square root of a number to find the
-	value of the hypotenuse. Does the Math object give us a function to find the square root?
 
-	Hint: We need to be able to calculate the square of a number to find the value
-	of the hypotenuse. Does the Math object give us a function find the square of a number?
-
-	Log the side1, side2 and side3 variables to the console (console.log(yourVariableName)). 
-	Check the console to make sure you're getting the desired result.
-*/
-
-/*
 	Part 4: (No coding is needed for this part).
 	We are given a list of 20 numbers. We have to go through the list and find the
 	largest number.
@@ -61,14 +67,14 @@
 /* Bonus
 Take a look at the numbers array below. Use the pseudo code you wrote above to find the largest number and log it to the console in the following format: `"The highest number is #"`.
 
-If you need a hint on how to solve this one, take a look at the steps listed below, along with the linked resources. 
+If you need a hint on how to solve this one, take a look at the steps listed below, along with the linked resources.
 
 Steps:
 - First create a variable highestNumber and give it a value of the number 0.
-- Then use a for loop(https://www.w3schools.com/js/js_loop_for.asp) to loop through the numbers array. 
+- Then use a for loop(https://www.w3schools.com/js/js_loop_for.asp) to loop through the numbers array.
 	Your for loop should look something like this:
 	for (var i = 0; i < numbers.length; i++) {
-	
+
 	}
 - Within the loop, use an if statement(https://www.w3schools.com/js/js_if_else.asp) to see if the current number (numbers[i]) is greater than the number that is currently stored in highestNumber.
 - If so, save numbers[i] in the highestNumber variable.
